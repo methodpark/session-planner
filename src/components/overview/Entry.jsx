d2b7e31c6127fc135b0e27aecbe5cfda4f88f1a9
Manuel Ernst <manuel.ernst@methodpark.de>
2018-07-20 12:06:19 +0200

    add room labels

--- src/components/overview/Entry.jsx
+++ src/components/overview/Entry.jsx
@@ -34,7 +34,7 @@ export default class Entry extends React.Component {
 
 export function TrackEntry({name, room, sessions}) {
   return (
-    <Entry title={name} subTitle={room} sessions={sessions}>
+    <Entry title={name} subTitle={'room: ' + room} sessions={sessions}>
       {session => <SessionWithTime key={session.id} {...session} />}
     </Entry>
   );
@@ -47,7 +47,7 @@ export function SlotEntry({start, end, sessions}) {
 
   return (
     <Entry title={title} sessions={sessions}>
-      {session => <SessionWithRoom key={session.id} room={session.track.room} {...session} />}
+      {session => <SessionWithRoom key={session.id} room={'room: ' + session.track.room} {...session} />}
     </Entry>
   );
 }
