74737ed6863af8b241a20ea219615fa120bf7c46
Frank Blendinger <Frank.Blendinger@methodpark.de>
2018-08-15 16:39:35 +0200

    Fix 'no-useless-escape' warning

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ function askPermission() {
 function urlBase64ToUint8Array(base64String) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
   const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
+    .replace(/-/g, '+')
     .replace(/_/g, '/');
 
   const rawData = window.atob(base64);
