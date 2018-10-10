7d20d9b5addf5556f6b9903618c1bc456392cc9f
Frank Blendinger <frank.blendinger@methodpark.de>
2018-10-10 20:14:23 +0200

    Server/notifications.js: re-indent / change EOL
    Other files use 2 spaces for indentation, and LF as EOL character.
    notifications.js used 4 spaces and CRLF. Not anymore it does.

--- srcServer/notifications.js
+++ srcServer/notifications.js
@@ -1,57 +1,57 @@
 const webpush = require('web-push');
 
 const vapidKeys = {
-    publicKey: 'BJRs5N-80wspERG5agPCaZILtDhX3f3DOF0iYu5URcNPNPcx2FCktXf0y9kIqpFgsTI_IXO-3JQHhhwIDsShorA',
-    privateKey: 'dIX0wUfayoY_mvIP0k8lNv2ICFMQOM9yR_YupRas3Eg'
+  publicKey: 'BJRs5N-80wspERG5agPCaZILtDhX3f3DOF0iYu5URcNPNPcx2FCktXf0y9kIqpFgsTI_IXO-3JQHhhwIDsShorA',
+  privateKey: 'dIX0wUfayoY_mvIP0k8lNv2ICFMQOM9yR_YupRas3Eg'
 };
 
 webpush.setVapidDetails(
-    'mailto:info@methodpark.de',
-    vapidKeys.publicKey,
-    vapidKeys.privateKey
+  'mailto:info@methodpark.de',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
 );
 
 let subscriptions = [];
 
 function isValidPushSubscription(request, response) {
-    // Check the request body has at least an endpoint.
-    if (!request.body || !request.body.endpoint) {
-        // Not a valid subscription.
-        response.status(400);
-        response.setHeader('Content-Type', 'application/json');
-        response.send(JSON.stringify({
-            error: {
-                id: 'no-endpoint',
-                message: 'Subscription must have an endpoint.'
-            }
-        }));
-        return false;
-    }
-    return true;
+  // Check the request body has at least an endpoint.
+  if (!request.body || !request.body.endpoint) {
+    // Not a valid subscription.
+    response.status(400);
+    response.setHeader('Content-Type', 'application/json');
+    response.send(JSON.stringify({
+      error: {
+        id: 'no-endpoint',
+        message: 'Subscription must have an endpoint.'
+      }
+    }));
+    return false;
+  }
+  return true;
 };
 
 function saveSubscriptionToArray(subscription) {
-    return new Promise(function (resolve, reject) {
+  return new Promise(function (resolve, reject) {
 
-        subscriptions.push(subscription);
+    subscriptions.push(subscription);
 
-        resolve(subscriptions.length - 1);
-    });
+    resolve(subscriptions.length - 1);
+  });
 };
 
 function deleteSubscriptionFromArray(id) {
-    subscriptions.splice(id, 1);
+  subscriptions.splice(id, 1);
 }
 
 function sendNotification(subscription, dataToSend) {
-    return webpush.sendNotification(subscription, dataToSend)
-        .catch((err) => {
-            if (err.statusCode === 410) {
-                return deleteSubscriptionFromArray(subscription._id);
-            } else {
-                console.log('Subscription is no longer valid: ', err);
-            }
-        });
+  return webpush.sendNotification(subscription, dataToSend)
+    .catch((err) => {
+      if (err.statusCode === 410) {
+        return deleteSubscriptionFromArray(subscription._id);
+      } else {
+        console.log('Subscription is no longer valid: ', err);
+      }
+    });
 };
 
 module.exports.isValidPushSubscription = isValidPushSubscription;
