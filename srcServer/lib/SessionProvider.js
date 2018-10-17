19af0cb383d9deb3fa58efab34d9e7515d4809ed
Michael Sandner <michaelsandy@gmx.de>
2018-10-17 11:33:39 +0200

    add basic file validation + add file watcher

--- srcServer/lib/SessionProvider.js
+++ srcServer/lib/SessionProvider.js
@@ -1,12 +1,65 @@
 const path = require('path');
 
+const fs = require('fs');
+
+const chokidar = require('chokidar');
+
 class SessionProvider {
   constructor(basePath) {
     this._basePath = basePath;
+    this._filePath = path.join(this._basePath, 'sessionsData.json');
+    this._currentData = null;
+
+    this.watchFileChange();
+  }
+
+  watchFileChange() {
+    chokidar.watch(this._filePath).on('change', (event, path) => {
+      console.log("file changed", event, path);
+      this._loadData();
+    });
   }
 
   async getSessions() {
-    return require(path.join(this._basePath, 'sessionsData.json'));
+    if (!this._currentData) {
+      await this._loadData();
+    }
+
+    return this._currentData;
+  }
+
+  async _loadData() {
+    return new Promise((resolve, reject) => {
+      fs.readFile(this._filePath, "utf8", (err, data) => {
+        if (err) {
+          reject(err);
+        }
+
+        try {
+          data = JSON.parse(data);
+          data = this._validate(data);
+          this._currentData = data;
+
+          console.log('successfully file reloaded');
+
+          resolve(data);
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  }
+
+  _validate(data) {
+    if (!Array.isArray(data)) {
+      throw new Error('must be an array');
+    }
+
+    if (data.length === 0) {
+      throw new Error('can not be empty');
+    }
+
+    return data;
   }
 }
 
