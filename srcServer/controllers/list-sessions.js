636b2817db2e797bdf679f879aa38cf3398daf02
Manuel Ernst <manuel.ernst@methodpark.de>
2018-10-17 16:45:28 +0200

    whitespaces

--- srcServer/controllers/list-sessions.js
+++ srcServer/controllers/list-sessions.js
@@ -1,7 +1,7 @@
 function factory(sessionProvider) {
   return async function listSessions(request, response) {
     const sessions = await sessionProvider.getSessions();
-  
+
     return response.json(sessions);
   }
 }
