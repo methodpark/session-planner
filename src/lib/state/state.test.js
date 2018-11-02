8ad44c2050ee5902de7064f42415d445e17562a0
Manuel Ernst <manuel.ernst@methodpark.de>
2018-11-02 13:44:42 +0100

    add tests for sessions, rooms and slots

--- src/lib/state/state.test.js
+++ src/lib/state/state.test.js
@@ -1,6 +1,8 @@
 import {
-  setFavorite,   SET_FAVORITE,
+  setFavorite, SET_FAVORITE,
   unsetFavorite, UNSET_FAVORITE,
+  addSession,
+  setActive,
   reducer
 } from './state';
 
@@ -28,117 +30,265 @@ describe("redux state handling", () => {
 
   describe("reducer", () => {
 
-    describe("when called with a setFavorite action", () => {
+    describe('setActive', () => {
+      const action = setActive('10:00 - 12:00');
 
-      const id = 42
-      const action = setFavorite(id);
+      it('should set the slot active that is active', () => {
+        const state = {
+          slots: [
+            { active: false, title: '10:00 - 12:00' },
+            { active: false, title: '12:00 - 14:00' },
+            { active: false, title: '14:00 - 16:00' }
+          ]
+        };
 
-      describe("when no favorites exist yet", () => {
-        it("stores the session id as favorite", () => {
-          const state = {};
+        const newState = reducer(state, action);
 
-          const newState = reducer(state, action);
-
-          expect(newState.favorites).toContain(id);
-        });
+        expect(newState.slots).toEqual([
+          { active: true, title: '10:00 - 12:00' },
+          { active: false, title: '12:00 - 14:00' },
+          { active: false, title: '14:00 - 16:00' }
+        ]);
       });
 
-      describe("when favorites are empty", () => {
-        it("stores the session id as favorite", () => {
-          const state = { favorites: [] };
+      it('should unset all other active slots', () => {
+        const state = {
+          slots: [
+            { active: false, title: '10:00 - 12:00' },
+            { active: false, title: '12:00 - 14:00' },
+            { active: true, title: '14:00 - 16:00' }
+          ]
+        };
 
-          const newState = reducer(state, action);
+        const newState = reducer(state, action);
 
-          expect(newState.favorites).toContain(id);
-        });
+        expect(newState.slots).toEqual([
+          { active: true, title: '10:00 - 12:00' },
+          { active: false, title: '12:00 - 14:00' },
+          { active: false, title: '14:00 - 16:00' }
+        ]);
       });
-
-      describe("when other favorites exist", () => {
-        it("stores the session id as favorite", () => {
-          const state = { favorites: [23, 1337] };
-
-          const newState = reducer(state, action);
-
-          expect(newState.favorites).toContain(id);
-        });
-      });
-
-      describe("when the id is already stored as one of many favorites", () => {
-        const state = { favorites: [23, id, 1337] };
-
-        it("keeps the id in the list of favorites", () => {
-          const newState = reducer(state, action);
-
-          expect(newState.favorites).toContain(id);
-        });
-
-        it("does not add the id a second time to the list", () => {
-          const initialNumberOfFavorites = state.favorites.length;
-
-          const newState = reducer(state, action);
-
-          expect(newState.favorites).toHaveLength(initialNumberOfFavorites);
-        });
-      });
-
     });
 
-    describe("when called with an unsetFavorite action", () => {
+    describe('add session', () => {
+      describe('rooms', () => {
+        const action = addSession(42, 'fooTitle', 'fooHost', 'fooRoom', '2018-01-01 10:00:00', '2018-01-01 12:00:00');
 
-      const id = 42
-      const action = unsetFavorite(id);
-
-      describe("when no favorites exist yet", () => {
-        it("initializes the favorites to an empty list", () => {
+        it('should add the room if there is none', () => {
           const state = {};
 
           const newState = reducer(state, action);
 
-          expect(newState.favorites).toEqual([]);
+          expect(newState.rooms).toEqual(['fooRoom']);
         });
-      });
 
-      describe("when favorites are empty", () => {
-        it("does nothing", () => {
-          const state = { favorites: [] };
+        it('should not add the room if it is there already', () => {
+          const state = { rooms: ['fooRoom'] };
 
           const newState = reducer(state, action);
 
-          expect(newState.favorites).toEqual(state.favorites);
+          expect(newState.rooms).toEqual(['fooRoom']);
         });
       });
 
-      describe("when other favorites exist", () => {
-        it("does nothing", () => {
-          const state = { favorites: [23, 1337] };
+      describe('slots', () => {
+        const action = addSession(42, 'fooTitle', 'fooHost', 'fooRoom', '2018-01-01 10:00:00', '2018-01-01 12:00:00');
+
+        it('should add the slot if there is none', () => {
+          const state = {};
 
           const newState = reducer(state, action);
 
-          expect(newState.favorites).toEqual(state.favorites);
+          const expected = [{
+            active: false,
+            title: '10:00 - 12:00',
+            start: '2018-01-01 10:00:00',
+            end: '2018-01-01 12:00:00'
+          }];
+          expect(newState.slots).toEqual(expected);
+        });
+
+        it('should not add the slot if its already there, compared by title', () => {
+          const state = {
+            slots: [{
+              active: true,
+              title: '10:00 - 12:00',
+              start: '2018-01-01 10:00:00',
+              end: '2018-01-01 12:00:00'
+            }]
+          };
+
+          const newState = reducer(state, action);
+
+          expect(newState.slots).toEqual([state.slots[0]]);
         });
       });
+    });
 
-      describe("when the id is stored as one of many favorites", () => {
-        it("removes the id from the list of favorites", () => {
+    describe('sessions', () => {
+      const action = addSession(42, 'fooTitle', 'fooHost', 'fooRoom', '2018-01-01 10:00:00', '2018-01-01 12:00:00');
+
+      it('should add the session if there is none', () => {
+        const state = {};
+
+        const newState = reducer(state, action);
+
+        const expected = [{
+          id: 42,
+          title: 'fooTitle',
+          host: 'fooHost',
+          room: 'fooRoom',
+          slot: '10:00 - 12:00',
+          start: '2018-01-01 10:00:00',
+          end: '2018-01-01 12:00:00'
+        }];
+        expect(newState.sessions).toEqual(expected);
+      });
+
+      it('should overwrite if a session with same id already exists', () => {
+        const state = {
+          sessions: [
+            {
+              id: 42,
+              title: 'barTitle',
+              host: 'barHost',
+              room: 'barRoom',
+              slot: '10:00 - 12:00',
+              start: '2018-01-01 10:00:00',
+              end: '2018-01-01 12:00:00'
+            }
+          ]
+        };
+
+        const newState = reducer(state, action);
+
+        const expected = [{
+          id: 42,
+          title: 'fooTitle',
+          host: 'fooHost',
+          room: 'fooRoom',
+          slot: '10:00 - 12:00',
+          start: '2018-01-01 10:00:00',
+          end: '2018-01-01 12:00:00'
+        }];
+        expect(newState.sessions).toEqual(expected);
+      });
+    });
+
+    describe('favs', () => {
+      describe("when called with a setFavorite action", () => {
+
+        const id = 42
+        const action = setFavorite(id);
+
+        describe("when no favorites exist yet", () => {
+          it("stores the session id as favorite", () => {
+            const state = {};
+
+            const newState = reducer(state, action);
+
+            expect(newState.favorites).toContain(id);
+          });
+        });
+
+        describe("when favorites are empty", () => {
+          it("stores the session id as favorite", () => {
+            const state = { favorites: [] };
+
+            const newState = reducer(state, action);
+
+            expect(newState.favorites).toContain(id);
+          });
+        });
+
+        describe("when other favorites exist", () => {
+          it("stores the session id as favorite", () => {
+            const state = { favorites: [23, 1337] };
+
+            const newState = reducer(state, action);
+
+            expect(newState.favorites).toContain(id);
+          });
+        });
+
+        describe("when the id is already stored as one of many favorites", () => {
           const state = { favorites: [23, id, 1337] };
 
-          const newState = reducer(state, action);
+          it("keeps the id in the list of favorites", () => {
+            const newState = reducer(state, action);
 
-          expect(newState.favorites).not.toContain(id);
+            expect(newState.favorites).toContain(id);
+          });
+
+          it("does not add the id a second time to the list", () => {
+            const initialNumberOfFavorites = state.favorites.length;
+
+            const newState = reducer(state, action);
+
+            expect(newState.favorites).toHaveLength(initialNumberOfFavorites);
+          });
         });
+
       });
 
-      describe("when the id is stored as the only favorites", () => {
-        it("empties the list of favorites", () => {
-          const state = { favorites: [id] };
+      describe("when called with an unsetFavorite action", () => {
 
-          const newState = reducer(state, action);
+        const id = 42
+        const action = unsetFavorite(id);
 
-          expect(newState.favorites).toEqual([]);
+        describe("when no favorites exist yet", () => {
+          it("initializes the favorites to an empty list", () => {
+            const state = {};
+
+            const newState = reducer(state, action);
+
+            expect(newState.favorites).toEqual([]);
+          });
         });
-      });
 
+        describe("when favorites are empty", () => {
+          it("does nothing", () => {
+            const state = { favorites: [] };
+
+            const newState = reducer(state, action);
+
+            expect(newState.favorites).toEqual(state.favorites);
+          });
+        });
+
+        describe("when other favorites exist", () => {
+          it("does nothing", () => {
+            const state = { favorites: [23, 1337] };
+
+            const newState = reducer(state, action);
+
+            expect(newState.favorites).toEqual(state.favorites);
+          });
+        });
+
+        describe("when the id is stored as one of many favorites", () => {
+          it("removes the id from the list of favorites", () => {
+            const state = { favorites: [23, id, 1337] };
+
+            const newState = reducer(state, action);
+
+            expect(newState.favorites).not.toContain(id);
+          });
+        });
+
+        describe("when the id is stored as the only favorites", () => {
+          it("empties the list of favorites", () => {
+            const state = { favorites: [id] };
+
+            const newState = reducer(state, action);
+
+            expect(newState.favorites).toEqual([]);
+          });
+        });
+
+      });
     });
+
 
   });
 
