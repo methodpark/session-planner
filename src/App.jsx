9390c1993fcd229d7e22df0d5cddca53e248f50d
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2018-11-06 15:28:42 +0100

    Add an overlay to tell users they can add this app to their home screen
    This is only displayed on ios devices and only if we're not already
    running inside a home screen app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 import AppRouter from './AppRouter';
+import {AddToHomeScreenPrompt} from './components/overview/AddToHomeScreenPrompt';
 
-const App = () => <AppRouter {...this.state} />;
+const App = () => <React.Fragment>
+  <AppRouter {...this.state} />
+  <AddToHomeScreenPrompt />
+</React.Fragment>;
 
 export default App;
