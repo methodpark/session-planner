35512ba35d5546ba493db802d30af104b2b09163
Tilman Adler <Tilman.Adler@methodpark.de>
2018-11-07 12:49:10 +0100

    Disable favorites filter when last favorite unset
    I.e. when the user has the filter active and removes all favorites
    the filter is automatically unset.

--- src/lib/state/sagas/favorites.js
+++ src/lib/state/sagas/favorites.js
@@ -1,7 +1,8 @@
-import { select, takeLatest } from 'redux-saga/effects';
+import { select, takeLatest, put } from 'redux-saga/effects';
 
 import { SET_FAVORITE, UNSET_FAVORITE } from '../state';
 import { storeFavorites } from '../../localStorage';
+import { setFavoritesFilter } from '../filterState';
 
 export function* watchFavorites() {
   yield takeLatest([SET_FAVORITE, UNSET_FAVORITE], handleFavoriteChange);
@@ -9,5 +10,10 @@ export function* watchFavorites() {
 
 function* handleFavoriteChange() {
   const favorites = yield select(state => state.favorites);
+
+  if(favorites.length === 0){
+    yield put(setFavoritesFilter(false));
+  }
+
   storeFavorites(favorites);
 }
