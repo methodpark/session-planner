2c4edd455393d644175f929caaa704f4b58dcb03
Tilman Adler <Tilman.Adler@methodpark.de>
2018-11-13 18:12:48 +0100

    Improve styling of menu

--- src/components/overview/Menu.jsx
+++ src/components/overview/Menu.jsx
@@ -11,6 +11,12 @@ const ReduxSlideMenu = reduxBurgerMenu(SlideMenu);
 const styles = {
   bmOverlay: {
     background: 'var(--background-primary)'
+  },
+  bmMenu: {
+    height: 'auto'
+  },
+  bmMenuWrap: {
+    width: 'auto'
   }
 }
 
@@ -18,10 +24,13 @@ export const Menu = () => (
   <ReduxSlideMenu styles={styles}>
     <a className="menuItem" href="#">Day 1</a>
     <a className="menuItem" href="#">Day 2</a>
-    <a className="menuItem" href="#">Day 3</a><br />
+    <a className="menuItem" href="#">Day 3</a>
+    <div className="divider" />
 
-    <a className="menuItem" href="#">Floor plan</a><br />
+    <a className="menuItem" href="#">Floor plan</a>
+    <div className="divider" />
 
     <ThemeSwitch />
+    <div className="divider" />
   </ReduxSlideMenu>
 )
