5e1af0875b05e8a26efca9fa117f9264ff4fa196
Michael Gerhaeuser <michael.gerhaeuser@methodpark.de>
2018-11-14 15:07:08 +0100

    Use svg instead of emoji in footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Heart from 'react-icons/lib/fa/heart';
 
 import Legal from './Legal';
+import './Footer.less';
 
 const OptionalLegal = () => (
   <Router>
@@ -14,7 +16,7 @@ const OptionalLegal = () => (
 
 const Footer = () => (
   <footer>
-    <p>Made with <span role="img" aria-label="love">❤️</span> by
+    <p>Made with <span role="img" aria-label="love"><Heart className="love" />️</span> by
       {' '}
       <a href="https://www.methodpark.de/mobileapps-webapps.html" target="_blank" rel="noopener noreferrer">
         <img src="/logo/mp.png" className="footer-logo-mp" alt="method park" />
