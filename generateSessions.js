d8c383b9fba3d348d14659d770c05943c0a8b222
mlgr <michael.gerhaeuser@methodpark.de>
2018-11-16 09:19:57 +0100

    Add a script that can generate a sessions template

---
+++ generateSessions.js
@@ -0,0 +1,29 @@
+const moment = require('moment');
+
+const rooms = ['BASIC', 'Clojure', 'Dylan', 'Eiffel', 'Forth', 'Groovy', 'Haskell', 'Idris', 'Julia'];
+const startOfDay = moment('2018-11-17 11:00:00');
+const endOfDay = moment('2018-11-17 18:00:00');
+const baseId = 'SWEC18-01';
+
+
+let start = startOfDay;
+const sessions = [];
+while (start.isBefore(endOfDay)) {
+  for (const room of rooms) {
+    const id = `${baseId}-0-${sessions.length.toString().padStart(3, '0')}`;
+    sessions.push({
+      id,
+      title: '',
+      host: '',
+      room,
+      start: start.format(),
+      end: moment(start).add(1, 'hour').format()
+    });
+  }
+  start.add(1, 'hour');
+  if (start.hour() === 13) {
+    start.add(1, 'hour');
+  }
+}
+
+console.log(JSON.stringify(sessions, null, 2));
